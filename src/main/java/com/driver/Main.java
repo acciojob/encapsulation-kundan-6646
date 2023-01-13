ea45306347d4020e12cd6a7d9dc6f6e4e6578327
unknown <kundanjha826@gmail.com>
2023-01-13 14:59:42 +0530

    soultion-1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Harry");
+        System.out.println(obj.getName());
+
+        // obj.name; name has private access
+    }
 }
